3c5a04ed891040c8b3d5d0bc1e826f1829df5bda
GXG <xiaoguang_guo@mines.edu>
2024-05-15 12:22:16 -0600

    blockchain

--- MCUs.m
+++
@@ -1,9 +0,0 @@
-function runMySimulation
-    fig = uifigure('Name', 'Simulation Controls');
-    startBtn = uibutton(fig,'push', 'Text', 'Start Simulation', 'Position',[100, 100, 100, 22], 'ButtonPushedFcn', @(btn,event) startSimulation());
-end
-
-function startSimulation()
-    disp('Simulation started');
-    % Insert code here to initialize and run your simulation
-end
