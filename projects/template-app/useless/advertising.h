3797dc6621a9ecee885e6f3018a4ef49730f1a0f
GXG <xiaoguang_guo@mines.edu>
2024-07-29 16:33:51 -0600

    distributed inference

--- projects/template-app/useless/advertising.h
+++
@@ -1,6 +0,0 @@
-#ifndef ADVERTISING_H
-#define ADVERTISING_H
-
-void start_advertising(void);
-
-#endif // ADVERTISING_H
--- projects/template-app/useless/display.h
+++
@@ -1,6 +0,0 @@
-#ifndef DISPLAY_H
-#define DISPLAY_H
-
-void display_device_info_task(void *pvParameters);
-
-#endif // DISPLAY_H
